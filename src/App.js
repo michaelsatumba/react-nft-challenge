79ccbd6c9fe201bcfed91af1581f3c5730523bdb
michaelsatumba <mjsatumba@gmail.com>
2021-12-18 13:20:45 -0800

    cyberPunk

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import './App.css';
-import CollectionCard from './components/CollectionCard';
 import Header from './components/Header';
+import { useEffect, useState } from 'react';
+import axios from "axios";
+import Punklist from './components/Punklist';
+
 
 
 function App() {
+ const [punkListData, setPunkListData] = useState([])
+
+ useEffect(() => {
+   const getMyNfts = async () => {
+     const openseaData = await axios.get(
+       "https://testnets-api.opensea.io/assets?asset_contract_address=0xc53C4266f4A9d1293e6C3a8b43788B6bB6a120E2&order_direction=asc"
+       )
+       console.log(openseaData.data.assets)
+       setPunkListData(openseaData.data.assets)
+   }
+
+   return getMyNfts()
+ }, [])
+
   return (
     <div className="app">
    <Header />
-   <CollectionCard 
-   id={0} 
-   name={"Bandana Punk"} 
-   traits={[{"value": 7}]} 
-   image="https://ipfs.thirdweb.com/ipfs/QmQAcLywRXMBs1YMumL29z9LEyPK3rJGcKKUAZjpYyKi6d" 
-   />
+   <Punklist punkListData={punkListData} />
    </div>
   );
 }
