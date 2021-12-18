a300ae04ac5d4a45cdaa3e5b9f4ba276b1067801
michaelsatumba <mjsatumba@gmail.com>
2021-12-18 12:15:21 -0800

    collectionCards

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import CollectionCard from './components/CollectionCard';
 import Header from './components/Header';
 
 
@@ -6,6 +7,12 @@ function App() {
   return (
     <div className="app">
    <Header />
+   <CollectionCard 
+   id={0} 
+   name={"Bandana Punk"} 
+   traits={[{"value": 7}]} 
+   image="https://ipfs.thirdweb.com/ipfs/QmQAcLywRXMBs1YMumL29z9LEyPK3rJGcKKUAZjpYyKi6d" 
+   />
    </div>
   );
 }
