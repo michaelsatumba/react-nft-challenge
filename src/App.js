dc83a0992985b03cc156cb0d056917e576f669b6
michaelsatumba <mjsatumba@gmail.com>
2021-12-19 10:31:21 -0800

    - return

--- src/App.js
+++ src/App.js
@@ -4,12 +4,32 @@ import { useEffect, useState } from 'react';
 import axios from "axios";
 import Punklist from './components/Punklist';
 import Main from './components/Main';
+import { ThirdwebSDK } from '@3rdweb/sdk';
 
 
 
 function App() {
   const [punkListData, setPunkListData] = useState([])
   const [selectedPunk, setSelectedPunk] = useState(0)
+  
+
+// useEffect(() => {
+//         const getMyNfts = async () => {
+//             // Web3 browser user detected. You can now use the provider.
+//             const sdk = new ThirdwebSDK('rinkeby');
+//             const nft = sdk.getNFTModule(
+//                 '0xc53C4266f4A9d1293e6C3a8b43788B6bB6a120E2'
+//             );
+//             console.log(nft);
+//             const owned = await nft.getAllWithOwner();
+//             console.log(owned);
+//             setPunkListData(owned);
+//         };
+//         return getMyNfts();
+//         // fetchData();
+//     }, []);
+
+  
 
   useEffect(() => {
     const getMyNfts = async () => {
@@ -20,7 +40,8 @@ function App() {
       setPunkListData(openseaData.data.assets)
     }
 
-    return getMyNfts()
+    // return
+     getMyNfts()
   }, [])
 
   return (
