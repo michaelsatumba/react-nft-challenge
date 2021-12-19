082a7d15b4fdf9af7abb6dd1894e9dad806fabea
michaelsatumba <mjsatumba@gmail.com>
2021-12-18 16:13:52 -0800

    punk

---
+++ src/components/Main.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Main = () => {
+    return (
+        <div className="main">
+            <div className="mainContent">
+                <div className="punkHighlight">
+                    <div className="punkContainer">
+                        <img className="punk" src="" alt="" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Main
